c355464e850b1646dab0de19f5dc43c1b4ba8c95
Vazi1999 <ofek1918@gmail.com>
2022-12-16 11:02:20 +0200

    16.12

--- fetch.py
+++ fetch.py
@@ -1,54 +1,66 @@
 from pip._vendor import requests
+import json
+import webbrowser
 
+def get_queryset(erorr):
+    String = ""
+    for word in erorr.split():
+        String += word + "%20"
+        
+    String = String[:-3]  # delete the last %20
+    erorr_query = "https://api.stackexchange.com/docs/search#order=desc&sort=votes&intitle=" + String + "&filter=default&site=stackoverflow"
+    return erorr_query
 
+def openStack(response):
+    data_json = json.loads(response)
+    counter = 0 
+    for item in data_json["items"]:
+        link = item["link"]
+        webbrowser.open(link, new=2) # open the link with the specified link.   
+        counter += 1
+        if counter == 3: # to open only 3 webpages
+            break   
 
+def fetch_stackoverflow(choice,user_input):
+    #params:
+    params = {
+            "site": "stackoverflow",
+            "client_id": "24988",
+            "client_secret": "2W7nr4j8WE8CNyY4)tUNGA((",
+            "key": "zSMudZtOD6STp9KlT6TLLg(("
+        }
+    if choice == 1: # write the problem yourself.
+        # Set the base URL for the Stack Exchange API
+        reqeust = get_queryset(user_input)
+        # Send the request to the API
+        response = requests.get(reqeust, params=params)
+        # Check if the request was successful
+        if response.status_code == 200:
+            openStack(response)
+        else:
+            print("Request didnt go as planned!" + response.status_code)
+    else:
+        try:
+        #need to pass the code to check it .
+            with open(user_input) as f:
+                code = f.read()
+                exec(code)
+        except FileNotFoundError:#couldnt find the filename.
+            print("File not found!")
+            exit(1)
+        except Exception as error:
+            error_message = str(error)
+            # Set the base URL for the Stack Exchange API
+            reqeust = get_queryset(error_message)
+            # Send the request to the API
+            response = requests.get(reqeust, params=params)
+            # Check if the request was successful
+            if response.status_code == 200:
+                openStack(response)
+            else:
+                print("Request didnt go as planned :( " + response.status_code)
+            
+            return True
 
-########################################################################
-
-
-try:
-#need to pass the code to check it .
-    x="https"
-    print(int(x))
-except Exception as error:
-    error_message = str(error)
-    print("The error is: " + error_message)
-    #second time################################################################
-    
-
-
-
-
-##################################################################
-
-## https://api.stackexchange.com/docs/search#order=desc&min=120&sort=votes&intitle=division%20by%20zero&filter=default&site=stackoverflow
-
-## https://api.stackexchange.com/docs/search#order=desc&min=120&sort=relevance&intitle=division%20by%20zero&filter=default&site=stackoverflow
-
-## https://api.stackexchange.com/docs/search#order=desc&sort=relevance&intitle=division%20by%20zero&filter=default&site=stackoverflow
-
-## https://api.stackexchange.com/docs/search#order=desc&sort=relevance&intitle=invalid%20literal%20for%20int()%20with%20base%2010&filter=default&site=stackoverflow
-
-
-
-# Set the base URL for the Stack Exchange API
-base_url = "https://api.stackexchange.com/2.3"
-
-# Set your API key
-api_key = "zSMudZtOD6STp9KlT6TLLg(("
-
-# Set the parameters for your query
-params = {
-    "site": "stackoverflow",
-    "client_id": "24988",
-    "client_secret": "2W7nr4j8WE8CNyY4)tUNGA((",
-    "key": api_key
-}
-
-# Send the request to the API
-response = requests.get(base_url + "/questions", params=params)
-
-# Print the response data
-print(response.json())
 
 
--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-#Changes
-########################################################################
+
+print("Would you like to run test on a script or write an error yourself ?\n")
+print("Please enter 0 for a script and 1 for a write error  ")
+choice = int(input())
+if choice == 0:
+    print("Enter locaition of the script to test")
+    script = input()
+    #call fetch_stackoverflow
+elif choice == 1:
+    print("Please enter the problem")
+    problem = input()
+    #call fetch_stackoverflow
+else:
+    print("Wrong input , exiting...")
+    exit(1)
+
+
+
+
